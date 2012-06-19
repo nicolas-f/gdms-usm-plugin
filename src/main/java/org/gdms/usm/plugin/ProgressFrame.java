9567e548e4d8813c404d1e180b012bd79157dba5
Antoine Gourlay <antoine@gourlay.fr>
2012-06-19 08:40:42 +0200

    update license headers to new legal format.

--- src/main/java/org/gdms/usm/plugin/ProgressFrame.java
+++ src/main/java/org/gdms/usm/plugin/ProgressFrame.java
@@ -1,6 +1,32 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * Gdms-USM is a library dedicated to multi-agent simulation for modeling urban sprawl.
+ * It is based on the GDMS library. It uses the OrbisGIS renderer to display results.
+ *
+ * This version is developed at French IRSTV Institute and at LIENSs UMR 7266 laboratory
+ * (http://lienss.univ-larochelle.fr/) as part of the VegDUD project, funded by the
+ * French Agence Nationale de la Recherche (ANR) under contract ANR-09-VILL-0007.
+ *
+ * Gdms-USM is distributed under GPL 3 license. It is maintained by the "Atelier SIG" team of
+ * the IRSTV Institute <http://www.irstv.fr/> CNRS FR 2488.
+ *
+ * Copyright (C) 2011-1012 IRSTV (FR CNRS 2488)
+ *
+ * Gdms-USM is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * Gdms-USM is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Gdms-USM. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * For more information, please consult: <http://trac.orbisgis.org/>
+ * or contact directly:
+ * info_at_ orbisgis.org
  */
 package org.gdms.usm.plugin;
 
