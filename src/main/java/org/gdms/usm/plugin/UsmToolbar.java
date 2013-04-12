7cc5bb116ef31e0cc4ed1895674cfc572170b8f2
nicolas-f <github@nettrader.fr>
2013-04-12 11:40:47 +0200

    update license

--- src/main/java/org/gdms/usm/plugin/UsmToolbar.java
+++ src/main/java/org/gdms/usm/plugin/UsmToolbar.java
@@ -1,28 +1,30 @@
-/*
- * OrbisGIS is a GIS application dedicated to scientific spatial simulation.
- * This cross-platform GIS is developed at French IRSTV institute and is able to
- * manipulate and create vector and raster spatial information.
+/**
  *
- * OrbisGIS is distributed under GPL 3 license. It is produced by the "Atelier SIG"
- * team of the IRSTV Institute <http://www.irstv.fr/> CNRS FR 2488.
+ * Gdms-USM is a library dedicated to multi-agent simulation for modeling urban sprawl.
+ * It is based on the GDMS library. It uses the OrbisGIS renderer to display results.
  *
- * Copyright (C) 2007-2012 IRSTV (FR CNRS 2488)
+ * This version is developed at French IRSTV Institute and at LIENSs UMR 7266 laboratory
+ * (http://lienss.univ-larochelle.fr/) as part of the VegDUD project, funded by the
+ * French Agence Nationale de la Recherche (ANR) under contract ANR-09-VILL-0007.
  *
- * This file is part of OrbisGIS.
+ * Gdms-USM is distributed under GPL 3 license. It is maintained by the "Atelier SIG" team of
+ * the IRSTV Institute <http://www.irstv.fr/> CNRS FR 2488.
  *
- * OrbisGIS is free software: you can redistribute it and/or modify it under the
+ * Copyright (C) 2011-2012 IRSTV (FR CNRS 2488)
+ *
+ * Gdms-USM is free software: you can redistribute it and/or modify it under the
  * terms of the GNU General Public License as published by the Free Software
  * Foundation, either version 3 of the License, or (at your option) any later
  * version.
  *
- * OrbisGIS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * Gdms-USM is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * OrbisGIS. If not, see <http://www.gnu.org/licenses/>.
+ * Gdms-USM. If not, see <http://www.gnu.org/licenses/>.
  *
- * For more information, please consult: <http://www.orbisgis.org/>
+ * For more information, please consult: <http://trac.orbisgis.org/>
  * or contact directly:
  * info_at_ orbisgis.org
  */
